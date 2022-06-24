2f9d7ec3144d299b76bf5741ba97f8d96044133f
ZhukovaSveta <thelumierlana@yandex.ru>
2022-06-24 16:20:35 +0300

    add tabBar

---
+++ 14/14/AlbomCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbomCollectionView.swift
+//  14
+//
+//  Created by Надежда Жукова on 16.06.2022.
+//
+
+import UIKit
+
+class AlbomCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 14/14/AlbomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AlbomCollectionViewCell.swift
+//  14
+//
+//  Created by Надежда Жукова on 16.06.2022.
+//
+
+import UIKit
+
+class AlbomCollectionViewCell: UICollectionViewCell {
+    
+}
